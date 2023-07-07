f98f8d41685b7809b6e66bda3dea6d3c3ff9d058
David Mak <david.18.19.21@gmail.com>
2023-07-08 03:06:08 +0800

    p_checksum: Fix mismatching braces

--- prboom2/src/p_checksum.h
+++ prboom2/src/p_checksum.h
@@ -4,9 +4,9 @@ extern "C" {
 
 extern void (*P_Checksum)(int);
 extern void P_ChecksumFinal(void);
-void P_RecordChecksum(const char *file);
-//void P_VerifyChecksum(const char *file);
+void P_RecordChecksum(const char* file);
+// void P_VerifyChecksum(const char *file);
 
 #ifdef __cplusplus
-extern "C" {
+}
 #endif  // __cplusplus
